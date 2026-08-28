355bcb09f044e62aceee4de3e0debbde75ad7cb0
ethanjordan2405-source <ethanjordan2405@gmail.com>
2026-08-28 17:06:13 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -244,7 +244,7 @@ export default function App() {
             </div>
 
             <BookingTable
-              rows={data.bookings}
+              rows={[...data.bookings].sort((a, b) => new Date(b.start_date) - new Date(a.start_date))}
               paid={paid}
               admin={admin}
               edit={x => setModal({ type: 'booking', x })}
@@ -269,7 +269,7 @@ export default function App() {
             </div>
 
             <BookingTable
-              rows={data.bookings}
+              rows={[...data.bookings].sort((a, b) => new Date(b.start_date) - new Date(a.start_date))}
               paid={paid}
               admin={admin}
               edit={x => setModal({ type: 'booking', x })}
